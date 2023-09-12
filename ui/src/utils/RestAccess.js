e1c240ae41238a8505aa2659081a7c54f68b47c5
thorikoshi19980820 <takaya.b.b0820@gmail.com>
2023-09-12 12:59:01 +0900

    restaccess修正

--- ui/src/utils/RestAccess.js
+++ ui/src/utils/RestAccess.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const customAxios = axios.create({baseURL: "http://192.168.0.192/api"});
-// const customAxios = axios.create({baseURL: "http://localhost/api"});
+// const customAxios = axios.create({baseURL: "http://192.168.0.192/api"});
+const customAxios = axios.create({baseURL: "http://localhost/api"});
 
 export async function get(url, options = {}) {
   return customAxios.get(url, {
